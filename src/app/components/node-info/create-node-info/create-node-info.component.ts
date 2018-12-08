94aa4773e4a16c89215a21f3a7d12c749149d60d
Phuc Trinh <trinhcanhphuc@gmail.com>
2018-12-08 22:08:38 +0700

    + create data in firebase

--- src/app/components/node-info/create-node-info/create-node-info.component.ts
+++ src/app/components/node-info/create-node-info/create-node-info.component.ts
@@ -27,22 +27,27 @@ export class CreateNodeInfoComponent implements OnInit {
   ) { }
 
   ngOnInit() {
-    this.requestParams = this.route.paramMap.destination._value;
+    // this.requestParams = this.route.paramMap.destination._value;
     // get params
-    let userId = 1;
-    let now = Date.now();
-    let date = formatDate(now, 'dd-MM-yyyy', 'en-US', '+700');
-    let time = formatDate(now, 'HH:mm:ss', 'en-US', '+700');
-    let nodeId = Number(this.requestParams.nodeId);
-    let soilMoisture =  Number(this.requestParams.soilMoisture);
-    let soilTemperature = Number(this.requestParams.soilTemperature);
-    let airHumidity = Number(this.requestParams.airHumidity);
-    let airTemperature = Number(this.requestParams.airTemperature);
+    // let userId = 1;
+    // let now = Date.now();
+    // let date = formatDate(now, 'dd-MM-yyyy', 'en-US', '+700');
+    // let time = formatDate(now, 'HH:mm:ss', 'en-US', '+700');
+    // let nodeId = Number(this.requestParams.nodeId);
+    // let soilMoisture =  Number(this.requestParams.soilMoisture);
+    // let soilTemperature = Number(this.requestParams.soilTemperature);
+    // let airHumidity = Number(this.requestParams.airHumidity);
+    // let airTemperature = Number(this.requestParams.airTemperature);
 
     // save into firebase
+    // this.nodeInfoService.createNodeInfo(1,
+    //   new NodeInfo(
+    //     `node${nodeId}`, date, time, soilMoisture, soilTemperature, airHumidity, airTemperature
+    //   ));
+
     this.nodeInfoService.createNodeInfo(1,
       new NodeInfo(
-        `node${nodeId}`, date, time, soilMoisture, soilTemperature, airHumidity, airTemperature
+        'node1', '10-10-2018', '19:00:00', 1, 1, 1, 1
       ));
   }
 }
